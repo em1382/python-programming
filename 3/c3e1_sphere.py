1bbf646ef123b22c3ef9482929ba1c309e45667c
Ellis Madagan <em1382@messiah.edu>
2016-07-15 15:03:42 -0400

    Update c3e1_sphere.py

--- 3/c3e1_sphere.py
+++ 3/c3e1_sphere.py
@@ -1,1 +1,15 @@
 # This program calculates the volume and surface area of a sphere.
+# Author: Ellis Madagan (em1382)
+
+import math
+
+def main():
+    radius = float(input("Enter the radius of a sphere: "))
+
+    volume = round((4/3) * math.pi * radius ** 3, 2)
+    area = round(4 * math.pi * radius ** 2, 2)
+
+    print("The volume is: {0}, and the surface area is: {1}.".format(volume, area))
+
+if __name__ == "__main__":
+    main()
